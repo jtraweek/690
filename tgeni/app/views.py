60ee3ee9c1b35e9ad4bc419d5736a0934c728d7e
WJPierro <wjpierro@gmail.com>
2017-03-05 00:49:20 -0500

    Added signin and registration views. Updated Jinja templates to use inheritance.

--- tgeni/app/views.py
+++ tgeni/app/views.py
@@ -6,7 +6,6 @@ from flask import (Response, flash, redirect, render_template,
                    request, url_for)
 from flask_login import (login_required, login_user, logout_user)
 
-
 @tgeni.route('/')
 def home():
     return redirect(url_for('index'))
@@ -15,27 +14,15 @@ def home():
 def index():
     return render_template('index.html')
 
-
 @tgeni.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
-        return Response('''
-            <h1>Please enter your registration credentials:</h1>
-            <form action="" method="post">
-                <p><input type=text      name=username placeholder="username"></p>
-                <p><input type=text      name=email    placeholder="email"></p>
-                <p><input type=password  name=password placeholder="password"></p>
-                <p><input type=submit    value="Register"></p>
-            </form>
-            ''')
-
+        return render_template('register.html')
     user = models.User(username = request.form['username'],
                         email    = request.form['email'],
                         password = request.form['password'])
-
     db.session.add(user)
     db.session.commit()
-
     flash('New user registered')
     return redirect(url_for('index'))
 
@@ -43,19 +30,10 @@ def register():
 def signin():
     if request.method == 'GET':
         # render the user login form
-        return Response('''
-            <h1>Please enter your login credentials:</h1>
-            <form action="" method="post">
-                <p><input type=text      name=username placeholder="username"></p>
-                <p><input type=password  name=password placeholder="password"></p>
-                <p><input type=submit    value="Sign In"></p>
-            </form>
-            ''')
-
+        return render_template('login.html')
     username = request.form['username']
     password = request.form['password']
     found_user = models.User.query.filter_by(username=username).first()
-
     if found_user and found_user.password_matches(password):
         login_user(found_user)
         flash('Logged in user')
@@ -71,11 +49,9 @@ def signout():
     logout_user()
     return redirect(url_for('index'))
 
-
 @login_manager.user_loader
 def load_user(id):
     return models.User.query.get(int(id))
-
 
 @tgeni.errorhandler(401)
 def fail_login(er):
