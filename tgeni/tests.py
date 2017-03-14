b0f05b7a26e90b58561f19a1c09756f6db3d9332
WJPierro <wjpierro@gmail.com>
2017-03-14 19:47:39 -0400

    Add model definitions to associate users with trips.

--- tgeni/tests.py
+++ tgeni/tests.py
@@ -1,4 +1,5 @@
 import app
+import app.models       as models
 import os
 import unittest
 import urllib
@@ -74,6 +75,45 @@ class TestCase(unittest.TestCase):
         self.assertEqual(found_user.username, test_username)
         self.assertEqual(found_user.email, test_email)
         self.assertTrue(found_user.password_matches(test_pw))
+
+    def test_user_trip_data(self):
+        # add users to db
+        user1 = models.User(username='George', email='user1@mail.web', password='pwd')
+        user2 = models.User(username='Dave', email='user2@mail.web', password='pwd')
+        user3 = models.User(username='Shelly', email='user3@mail.web', password='pwd')
+        user4 = models.User(username='Bart', email='user4@mail.web', password='pwd')
+        app.db.session.add(user1)
+        app.db.session.add(user2)
+        app.db.session.add(user3)
+        app.db.session.add(user4)
+        app.db.session.commit()
+        # add trips to db
+        trip1 = models.Trip(trip_name='New York', trip_length=7)
+        trip2 = models.Trip(trip_name='Disneyworld', trip_length=13)
+        app.db.session.add(trip1)
+        app.db.session.add(trip2)
+        app.db.session.commit()
+        # add users to trips
+        trip1.users.append(user1)
+        trip1.users.append(user3)
+        trip1.users.append(user4)
+        trip2.users.append(user2)
+        trip2.users.append(user4)
+        app.db.session.commit()
+        # check that users were added to trips
+        found_trip_1 = app.models.Trip.query.filter_by(trip_name='New York').first()
+        found_trip_2 = app.models.Trip.query.filter_by(trip_name='Disneyworld').first()
+        self.assertTrue(user1 in found_trip_1.users)
+        self.assertFalse(user2 in found_trip_1.users)
+        self.assertTrue(user3 in found_trip_1.users)
+        self.assertTrue(user4 in found_trip_1.users)
+        self.assertFalse(user1 in found_trip_2.users)
+        self.assertTrue(user2 in found_trip_2.users)
+        self.assertFalse(user3 in found_trip_2.users)
+        self.assertTrue(user4 in found_trip_2.users)
+
+
+
 
 
 if __name__ == '__main__':
