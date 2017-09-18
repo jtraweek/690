8062d73f8e9fc0729d0e6febd9d9a31dfab8487d
WJPierro <wjpierro@gmail.com>
2017-09-18 08:36:12 -0400

    Split test cases into subclasses for each view

--- tgeni/tests.py
+++ tgeni/tests.py
@@ -11,6 +11,7 @@ from unittest       import TestCase
 from flask_login    import current_user, AnonymousUserMixin
 
 
+# --------------------------------------------------------------------------
 class BaseTestCase(TestCase):
     """ Some base test case methods to be used for all test cases.
     """
@@ -26,15 +27,14 @@ class BaseTestCase(TestCase):
     def tearDown(self):
         pass
 
-    #=============================================
-    #   Some helper functions
-    #
+
     def register(self, username, email, password):
         return self.client.post(
             '/register',
             data=dict(username=username, email=email, password=password),
             follow_redirects=True
             )
+
 
     def login(self, username, password):
         return self.client.post(
@@ -43,18 +43,18 @@ class BaseTestCase(TestCase):
             follow_redirects=True
             )
 
+
     def logout(self):
         return self.client.get(
             '/signout',
             follow_redirects=True
             )
-    #=============================================
 
 
-
-
-class ViewTestCase(BaseTestCase):
-
+# --------------------------------------------------------------------------
+class IndexViewTestCase(BaseTestCase):
+    """
+    """
     def test_index(self):
         """ Test that the main index page renders.
         """
@@ -62,11 +62,29 @@ class ViewTestCase(BaseTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('Travel Geni', str(response.data))
 
+
+# --------------------------------------------------------------------------
+class IndexViewTestCase(BaseTestCase):
+    """
+    """
+    def test_index_renders(self):
+        """ Test that the main index page renders.
+        """
+        response =  self.client.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Travel Geni', str(response.data))
+
+
+# --------------------------------------------------------------------------
+class RegisterViewTestCase(BaseTestCase):
+    """
+    """
     def test_register_renders(self):
         response =  self.client.get('/register', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         self.assertIn('Register', str(response.data))
         self.assertIn('Sign Up', str(response.data))
+
 
     def test_register(self):
         """ Test that a valid registration works.
@@ -79,10 +97,12 @@ class ViewTestCase(BaseTestCase):
         self.assertEqual(user.email, 'testuser@email.web')
         self.assertTrue(user.password_matches('testpwd'))
 
+
     def test_register_username_taken(self):
          self.register(username='test', email='testuser@email.web', password='testpwd')
          response = self.register(username='test', email='testuser@email.web', password='testpwd')
          self.assertIn('Username already take', str(response.data))
+
 
     def test_register_missing_input(self):
         """ Test register with required fields left blank.
@@ -94,16 +114,23 @@ class ViewTestCase(BaseTestCase):
         response = self.register(username='test', email='testuser@email.web', password='')
         self.assertIn('This field is required.', str(response.data))
 
+
     def test_register_missing_input(self):
         response = self.register(username='test', email='invalidemail', password='testpwd')
         self.assertIn('Invalid email address.', str(response.data))
 
+
+# --------------------------------------------------------------------------
+class SigninViewTestCase(BaseTestCase):
+    """
+    """
     def test_signin_renders(self):
         response =  self.client.get('/signin', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         self.assertIn('Sign In', str(response.data))
         self.assertIn('Username', str(response.data))
         self.assertIn('Password', str(response.data))
+
 
     def test_signin(self):
         """ Test that a user can sign in with valid credentials.
@@ -115,6 +142,7 @@ class ViewTestCase(BaseTestCase):
             self.assertIn('testuser', str(response.data))
             self.assertTrue(current_user.is_authenticated)
 
+
     def test_signin_invalid_username(self):
         """ Test signin with a nonexistent user.
         """
@@ -122,6 +150,7 @@ class ViewTestCase(BaseTestCase):
             response = self.login('testuser', 'testpwd')
             self.assertIn('Invalid username or password', str(response.data))
             self.assertFalse(current_user.is_authenticated)
+
 
     def test_signin_invalid_password(self):
         """ Test signin with mismatched password.
@@ -131,6 +160,7 @@ class ViewTestCase(BaseTestCase):
             response = self.login('testuser', 'wrongpwd')
             self.assertIn('Invalid username or password', str(response.data))
             self.assertFalse(current_user.is_authenticated)
+
 
     def test_signin_missing_input(self):
         """ Test signin with required fields left blank.
@@ -146,6 +176,11 @@ class ViewTestCase(BaseTestCase):
             self.assertIn('This field is required.', str(response.data))
             self.assertFalse(current_user.is_authenticated)
 
+
+# --------------------------------------------------------------------------
+class SignoutViewTestCase(BaseTestCase):
+    """
+    """
     def test_signout(self):
         """ Test that a user can sign out properly.
         """
@@ -157,9 +192,10 @@ class ViewTestCase(BaseTestCase):
             self.assertFalse(current_user.is_authenticated)
 
 
-
+# --------------------------------------------------------------------------
 class ErrorHandlerTestCase(BaseTestCase):
-
+    """ Test HTTP error message pages.
+    """
     def test_404(self):
         response =  self.client.get('/nonexistent')
         self.assertIn('Oh no, 404!', str(response.data))
