abfbfed37c3b32fd42c5fd8884dd904cf92e27fe
WJPierro <wjpierro@gmail.com>
2017-10-16 13:40:03 -0400

    Add unit tests for change password.

--- tgeni/test/views/test_profile_views.py
+++ tgeni/test/views/test_profile_views.py
@@ -51,3 +51,89 @@ class EditProfileViewTestCase(BaseTestCase):
             self.assertIn(new_data['username'], str(response.data))
             self.assertIn(new_data['email'],    str(response.data))
             self.assertIn(new_data['bio'],      str(response.data))
+
+
+class ChangePasswordViewTestCase(BaseTestCase):
+    """
+    """
+    def test_change_password_signin_required(self):
+        """ Try to access the page while not logged in.
+        """
+        self.register(username='testuser', email='testuser@email.web', password='testpwd')
+        with self.client:
+            response =  self.client.get('/change_password', follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('Sign In', str(response.data))
+            self.assertIn('Username', str(response.data))
+            self.assertIn('Password', str(response.data))
+
+    def test_change_password_renders(self):
+        user = dict(
+            username='testuser',
+            email='testuser@email.web',
+            password='testpwd')
+        self.register(**user)
+        with self.client:
+            response = self.login(username=user['username'], password=user['password'])
+            response = self.client.get('/change_password', follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('Change Password', str(response.data))
+            self.assertIn('New Password', str(response.data))
+            self.assertIn('Confirm New Password', str(response.data))
+
+    def test_change_password_change(self):
+        old_password = 'oldpwd'
+        new_password = 'newpwd'
+        username='testuser'
+        self.register(username=username, email='someemail@email.email', password=old_password)
+        with self.client:
+            self.login(username=username, password=old_password)
+            response = self.client.post('/change_password',
+                                        follow_redirects=True,
+                                        data=dict(
+                                            password=new_password,
+                                            confirm=new_password
+                                        ))
+            self.assertIn('Change Password', str(response.data))
+            self.assertIn('New Password', str(response.data))
+            self.assertIn('Confirm New Password', str(response.data))
+            #
+            self.logout()
+            self.login(username=username, password=new_password)
+            self.assertEqual(current_user.username, username)
+
+    def test_change_password_missing_password(self):
+        self.register(username='testuser', email='someemail@email.email', password='pwd')
+        with self.client:
+            self.login(username='testuser', password='pwd')
+            response = self.client.post('/change_password',
+                                        follow_redirects=True,
+                                        data=dict(
+                                            password='',
+                                            confirm='newpwd'
+                                        ))
+            self.assertIn('This field is required.', str(response.data))
+
+    def test_change_password_missing_confirm(self):
+        self.register(username='testuser', email='someemail@email.email', password='pwd')
+        with self.client:
+            self.login(username='testuser', password='pwd')
+            response = self.client.post('/change_password',
+                                        follow_redirects=True,
+                                        data=dict(
+                                            password='newpwd',
+                                            confirm=''
+                                        ))
+            self.assertIn('This field is required.', str(response.data))
+
+    def test_change_password_mismatch(self):
+        self.register(username='testuser', email='someemail@email.email', password='pwd')
+        with self.client:
+            self.login(username='testuser', password='pwd')
+            response = self.client.post('/change_password',
+                                        follow_redirects=True,
+                                        data=dict(
+                                            password='newpwd',
+                                            confirm='diffnewpwd'
+                                        ))
+            self.assertIn('Passwords must match', str(response.data))
